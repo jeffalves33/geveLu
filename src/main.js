8799772875e2020a7ff9b66b8f00be45756e86fc
Jeferson <luiz_cleidi@hotmail.com>
2025-08-09 14:00:41 -0300

    v1.0.0

--- src/main.js
+++ src/main.js
@@ -12,7 +12,8 @@ function createWindow() {
             contextIsolation: false,
             enableRemoteModule: true,
         },
-        icon: path.join(__dirname, "assets/icon.png"),
+        //icon: path.join(__dirname, "assets/icon.png"),
+        icon: path.join(__dirname, 'assets', 'icons', 'simbolo.png'),
         show: false,
     })
 
